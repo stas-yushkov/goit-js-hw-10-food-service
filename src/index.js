13a40852f46bba2fc70231015b5955317c363e2e
stas-yushkov <stas.yushkov@gmail.com>
2021-06-25 00:33:55 +0300

    add markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+import template from './templates/template.hbs';
+import menuData from './menu.json';
+
+console.log('🚀 ~ menuData', menuData);
+
+window.onload = () => {
+  const menuRef = document.querySelector('.js-menu');
+  console.log('🚀 ~ menuRef', menuRef);
+
+  menuRef.innerHTML = template(menuData);
+};
