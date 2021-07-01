bf9a261b11f065c41044bc1c36ba13491703a2da
stas-yushkov <stas.yushkov@gmail.com>
2021-07-01 22:57:51 +0300

    add 'Сервис для localStorage'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 import './sass/main.scss';
 import template from './templates/template.hbs';
 import menuData from './menu.json';
+import storage from './js/utils/storage';
 
 const body = document.querySelector('body');
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+const CHECKED_THEME_STATE = 'checkedThemeState';
 
 const menuRef = document.querySelector('.js-menu');
 menuRef.innerHTML = template(menuData);
 
-const isThemeSet = localStorage.getItem('checkedThemeState');
+const isThemeSet = storage.load(CHECKED_THEME_STATE);
+// const isThemeSet = storage.load('checkedThemeState');
 const themeSwitcherRef = document.querySelector('#theme-switch-toggle');
 
-let checkedThemeState;
+let newThemeState;
 
 if (!isThemeSet) {
-  checkedThemeState = Theme.LIGHT;
-  body.classList = checkedThemeState;
+  newThemeState = Theme.LIGHT;
+  body.classList = newThemeState;
   // themeSwitcherRef.checked = false;
 } else {
-  checkedThemeState = isThemeSet;
-  body.classList = checkedThemeState;
+  newThemeState = isThemeSet;
+  body.classList = newThemeState;
 
-  if (checkedThemeState === Theme.LIGHT) {
+  if (newThemeState === Theme.LIGHT) {
     themeSwitcherRef.checked = false;
   } else {
     themeSwitcherRef.checked = true;
@@ -32,7 +35,7 @@ if (!isThemeSet) {
 }
 
 themeSwitcherRef.addEventListener('change', e => {
-  checkedThemeState = !e.target.checked ? 'light-theme' : 'dark-theme';
-  body.classList = checkedThemeState;
-  localStorage.setItem('checkedThemeState', checkedThemeState);
+  newThemeState = !e.target.checked ? 'light-theme' : 'dark-theme';
+  body.classList = newThemeState;
+  storage.save(CHECKED_THEME_STATE, newThemeState);
 });
